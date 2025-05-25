74d2cef2fba9351e21e82c48bb6f087918a2c9c2
Steelwix <mhunmael@hotmail.com>
2025-05-25 17:10:43 +0200

    Issues 1 - 2 - 3 - 4

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\UX\Turbo\TurboBundle;
 
 final class HomeController extends AbstractController
 {
@@ -13,6 +14,14 @@ final class HomeController extends AbstractController
     public function index(Request $request): Response
     {
         return $this->render('home/index.html.twig');
+
+    }
+
+    #[Route('/home/about', name: 'app_home_about')]
+    public function homeAbout(Request $request): Response
+    {
+        $request->setRequestFormat(TurboBundle::STREAM_FORMAT);
+        return $this->render('home/about.html.twig');
 
     }
 }
